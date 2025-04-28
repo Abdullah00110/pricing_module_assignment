04b2ccfe08b6d40b3a7e603bb3184b0092d0e4bd
unknown <abdullahsunasara6@gmail.com>
2025-04-28 20:42:23 +0530

    Registered PricingConfig and PricingConfigLog models in Django Admin with basic customization

--- pricing_app/admin.py
+++ pricing_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import PricingConfig, PricingConfigLog
 
-# Register your models here.
+@admin.register(PricingConfig)
+class PricingConfigAdmin(admin.ModelAdmin):
+    list_display = ('day_of_week', 'base_price', 'distance_limit_km', 'is_active')
+    list_filter = ('day_of_week', 'is_active')
+    search_fields = ('day_of_week',)
+
+@admin.register(PricingConfigLog)
+class PricingConfigLogAdmin(admin.ModelAdmin):
+    list_display = ('config', 'action', 'actor', 'timestamp')
+    list_filter = ('action',)
+    search_fields = ('actor',)
